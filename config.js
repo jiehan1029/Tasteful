ca297a430936009d23fd60f4692eee71a579a78d
Jie Han <jiehan1029@gmail.com>
2018-06-03 12:49:44 -0700

    setup heroku

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 // use mlab database for consistency
-exports.DATABASE_URL = 'mongodb://admin:admin@ds127490.mlab.com:27490/thinkful-capstone-2-app';
-exports.DATABASE_URL = 'mongodb://testAdmin1:testAdmin1@ds147030.mlab.com:47030/thinkful-capstone2-test';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/recipe-app'
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/recipe-app-test';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
