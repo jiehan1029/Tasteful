a14505880c04ff4c59e1ee3a019f8fd316de11e3
Jie Han <jiehan1029@gmail.com>
2018-06-03 13:26:55 -0700

    add api key

--- test/recipes.test.js
+++ test/recipes.test.js
@@ -4,6 +4,7 @@ const chaiHttp = require('chai-http');
 const {app, runServer, closeServer} = require('../server');
 chai.use(chaiHttp);
 const expect = chai.expect;
+const {MASHAPE_KEY} = require('../config');
 
 describe('test recipes endpoint',function(){
 
@@ -12,6 +13,7 @@ describe('test recipes endpoint',function(){
 			let res;
 			return chai.request(app)
 			.get(routeToTest)
+      .set('X-Mashape-Key', MASHAPE_KEY)
       .then(function(_res) {
         res = _res;
         expect(res).to.have.status(200);
@@ -24,6 +26,7 @@ describe('test recipes endpoint',function(){
       let res;
       return chai.request(app)
       .get(routeToTest)
+      .set('X-Mashape-Key', MASHAPE_KEY)
       .then(function(_res) {
         res = _res;
         expect(res).to.have.status(200);
